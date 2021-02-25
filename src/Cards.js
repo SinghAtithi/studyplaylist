f6037d100c05774b825d3452b3f5e84dc6199e9c
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-02-25 23:26:31 +0530

    added

---
+++ src/Cards.js
@@ -0,0 +1,26 @@
+// import React from 'React';
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Cards.css";
+function Cards(props) {
+  console.log("working");
+
+  return (
+    <div className="cards">
+      {/* <a href={props.link} target="blank"> */}
+        <div className="card" style={{ backgroundColor: `${props.color}` }}>
+          <div className="text">
+            <div className="logo">
+              <h1>{props.name}</h1>
+            </div>
+            <h4>By {props.creator} </h4>
+          </div>
+        </div>
+        <a href={props.link} target="blank">
+        <Button className="btn" variant="secondary">Watch Now!</Button>{""}
+      </a>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Cards from "./Cards";
+import "./index.css";
+
+ReactDOM.render(
+  <>
+    <div className="title">
+      <h1>My Youtube Playlists!</h1>
+    </div>
+    <Cards name="React" color="#283593 " link="https://www.youtube.com/playlist?list=PLwGdqUZWnOp3aROg4wypcRhZqJG3ajZWJ" creator="Thapa Technical" />
+
+    <Cards name="Rucursion" color="#C62828" link="https://www.youtube.com/playlist?list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY" creator="Aditya Verma" />
+
+    <Cards name="Dynamic-Prog" color="#37474F" link="https://www.youtube.com/playlist?list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go" creator="Aditya Verma" />
+
+    <Cards name="NodeJs" color="#43A047" link="https://www.youtube.com/playlist?list=PLwGdqUZWnOp00IbeN0OtL9dmnasipZ9x8" creator="Thapa Technical" />
+
+    <Cards name="Unacademy CP" color="#1976D2" link="https://unacademy.com/@healthyug" creator="Demoralizer" />
+
+    <Cards name="My-Github" color="#000000" link="https://github.com/SinghAtithi" creator="Of-course Me!" />
+  </>,
+  document.getElementById("root")
+);
