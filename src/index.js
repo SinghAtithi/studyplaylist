f46982014147ed91a69d2a0b8cdf2b46cb009d5f
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-02-27 23:50:59 +0530

    added 2 playlist

--- src/index.js
+++ src/index.js
@@ -17,11 +17,14 @@ ReactDOM.render(
 
     <Cards name="NodeJs" color="#43A047" link="https://www.youtube.com/playlist?list=PLwGdqUZWnOp00IbeN0OtL9dmnasipZ9x8" creator="Thapa Technical" />
 
-    <Cards name="Unacademy CP" color="#1976D2" link="https://unacademy.com/@healthyug" creator="Demoralizer" />
+    <Cards name="Bit Manup." color="#e57373" link="https://www.youtube.com/playlist?list=PL2q4fbVm1Ik7ip1VkWwe5U_CEb93vw6Iu" creator="Code NCode" />
 
+    <Cards name="Number Theory" color="#31394D" link="https://www.youtube.com/playlist?list=PL2q4fbVm1Ik4liHX78IRslXzUr8z5QxsG" creator="Code NCode" />
+
+    <Cards name="Unacademy CP" color="#1976D2" link="https://unacademy.com/@healthyug" creator="Demoralizer" />
+    
     <Cards name="My-Github" color="#000000" link="https://github.com/SinghAtithi" creator="Of-course Me!" />
 
-    {/* <Cards name="google" color="#458654" link="https://github.com/SinghAtithi" creator="Of-course Me!" /> */}
   </>,
   document.getElementById("root")
 );
